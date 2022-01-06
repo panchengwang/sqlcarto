4126460e7161785d5b5413b23920da3d1bacfe3b
pcwang （麓山老将） <wang_wang_lao@163.com>
2022-01-06 22:01:17 +0800

    sc_has_column判断列是否存在，sc_add_id_column增加id字段（uuid）

--- pg_utils.sql
+++ pg_utils.sql
@@ -17,5 +17,38 @@ $$
 $$
 language 'sql';
 
-select sc_get_all_partitions('public','长沙');
 
+create or replace function sc_has_column(
+  schemaname varchar,
+  tablename varchar,
+  columnname varchar
+) returns boolean as $$
+  select 
+    count(1) >=1 
+  from 
+    information_schema.columns
+  where 
+    table_schema=$1 and table_name=$2 and column_name=$3;
+$$ language 'sql';
+
+
+-- 给表增加一个_id字段
+-- _id 字符串，长度32，uuid去掉中间的连接符
+-- 如果表中已经包含_id字段，则什么也不做
+create or replace function sc_add_id_column(
+  schemaname varchar,
+  tablename varchar
+) returns varchar as $$
+declare
+  sqlstr text;
+begin
+  if sc_has_column(schemaname, tablename, '_id') then
+    raise notice '%','column _id already exists!';
+    return '_id';
+  end if;
+  sqlstr := 'alter table ' || quote_ident(schemaname) || '.' || quote_ident(tablename) || ' 
+    add column _id varchar(32) default sc_uuid()';
+  execute sqlstr;
+  return '_id';
+end;
+$$ language 'plpgsql';
