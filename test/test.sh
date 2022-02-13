6d5b5313206e26a6710496401e3f38056ce99cdf
pcwang （麓山老将） <wang_wang_lao@163.com>
2022-02-14 00:35:36 +0800

    测试createtopology修改

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-CUR_DIR=$(cd `dirname $0`; pwd)
+EXTENSION_DIR=`pg_config --sharedir`/extension
+CONTROL_FILE=$EXTENSION_DIR/postgis_topology.control
+POSTGIS_VER=`grep 'default_version' $CONTROL_FILE | sed "s/.*'\(.*\)'/\1/g"`
+SQL_FILE=${EXTENSION_DIR}/postgis_topology--${POSTGIS_VER}.sql
+if [ ! -f ${SQL_FILE}.bak ]; then
+  sudo cp $SQL_FILE ${SQL_FILE}.bak
+fi
+sudo cp -f ${SQL_FILE}.bak ${SQL_FILE}
+sudo sh -c "echo '\n\r' >> ${SQL_FILE}"
+sudo sh -c "cat ../postgis_topology_patch.sql >> ${SQL_FILE}"
 
-DBNAME=sqlcarto
-dropdb $DBNAME
-createdb $DBNAME
-
-psql -d $DBNAME -c "create extension \"uuid-ossp\";"
-psql -d $DBNAME -c "create extension postgis;"
-psql -d $DBNAME -c "create extension postgis_sfcgal;"
-psql -d $DBNAME -c "create extension postgis_topology;"
-psql -d $DBNAME -c "create extension sqlcarto;"
-psql -d $DBNAME -f $CUR_DIR/data/testdata.sql
-psql -d $DBNAME -f $CUR_DIR/test.sql
